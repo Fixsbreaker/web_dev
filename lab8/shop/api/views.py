f6616f73f11c29ca1c8de90abf3813f7f711efec
Fixsbreaker <alnurkunbolov@mail.ru>
2025-03-26 10:40:11 +0500

    Change mistakes

--- lab8/shop/api/views.py
+++ lab8/shop/api/views.py
@@ -1,37 +1,24 @@
-from django.http.response import JsonResponse
-
-from .models import *
-
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+from .models import Product, Category
 
 def products_list(request):
-    products = Product.objects.all()
-    products_json = [p.to_json() for p in products]
-    return JsonResponse(products_json, safe=False)
+    products = Product.objects.values()
+    return JsonResponse(list(products), safe=False)
 
 def get_product(request, id):
-    try:
-        product = Product.objects.get(id=id)
-        return JsonResponse(product.to_json()  , safe=False)
-    except Product.DoesNotExist:
-        return JsonResponse({'error': 'Product not found'}, status=404)
+    product = get_object_or_404(Product, id=id)
+    return JsonResponse(product.to_json(), safe=False)
 
 def categories_list(request):
-    categories = Category.objects.all()
-    categories_json = [category.to_json() for category in categories]
-    return JsonResponse(categories_json, safe=False)
+    categories = Category.objects.values()
+    return JsonResponse(list(categories), safe=False)
 
 def get_category(request, id):
-    try:
-        category = Category.objects.get(id=id)
-        return JsonResponse(category.to_json(), safe=False)
-    except Category.DoesNotExist:
-        return JsonResponse({'error': 'Category not found'}, status=404)
+    category = get_object_or_404(Category, id=id)
+    return JsonResponse(category.to_json(), safe=False)
 
 def get_products_by_category(request, id):
-    try:
-        category = Category.objects.get(id=id)
-        products = Product.objects.all().filter(category=category)
-        products_json = [p.to_json() for p in products]
-        return JsonResponse(products_json, safe=False)
-    except Category.DoesNotExist:
-        return JsonResponse({'error': 'Category not found'}, status=404)
+    category = get_object_or_404(Category, id=id)
+    products = Product.objects.filter(category=category).values()
+    return JsonResponse(list(products), safe=False)
